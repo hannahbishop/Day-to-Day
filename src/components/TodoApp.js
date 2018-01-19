2883d9d137d3b8248eb554c3ca8281c539a2bb35
hannahbishop <hannahbananabishop@gmail.com>
2018-01-18 21:17:29 -0800

    add local storage

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,21 +1,50 @@
 import React from 'react';
 import TodoPanel from './TodoPanel';
 
-let id = 0;
+//let id = 0;
+
 class TodoApp extends React.Component {
 
   constructor(props){
     super(props);
+    const localData = JSON.parse(localStorage.getItem("todos"));
     this.state = {
-      data: []
+      data: localData || []
     };
+    this.addTodo = this.addTodo.bind(this);
+		this.handleCheckbox = this.handleCheckbox.bind(this);
   }
 
+	updateLocalStorage() {
+		if (typeof(Storage) !== "undefined")
+			localStorage.setItem("todos", JSON.stringify(this.state.data));
+  }
+  
   addTodo(val, isComplete) {
     if (val !== '') {
-      const todo = { text: val, isComplete: isComplete, id: id++ }
+      let id;
+      if (typeof(Storage) !== "undefined") {
+        id = parseInt(localStorage.getItem("count"));
+      } else {
+        id = window.id;
+      }
+      const todo = { 
+        text: val, 
+        isComplete: isComplete, 
+        id: id
+      }
       this.state.data.unshift(todo);
-      this.setState({ data: this.state.data });
+      this.setState({
+        data: this.state.data
+      }, () => {
+        this.updateLocalStorage();
+      });
+      if (typeof(Storage) !== "undefined") {
+        id++;
+        localStorage.setItem("count", String(id));
+      } else {
+        window.id++;
+      }
     }
   }
 
@@ -28,18 +57,33 @@ class TodoApp extends React.Component {
     });
     toAdd.map((todo) => {
       todo.isComplete = !todo.isComplete;
-      let isComplete = todo.isComplete ? true : false;
-      this.state.data.splice(this.state.data.indexOf(todo), 1); //delete this completed todo
-      this.addTodo(todo.text, isComplete); //re-add to the top of the data array
+      this.state.data.splice(0, 0, this.state.data.splice(this.state.data.indexOf(todo), 1)[0]);
+      this.setState({
+        data: this.state.data
+      }, () => {
+        this.updateLocalStorage();
+      });
     });
+  }
+
+  componentDidMount() {
+		if (typeof(Storage) !== "undefined") {
+			localStorage.setItem("todos", JSON.stringify(this.state.data));
+      if(!localStorage.getItem("count")) {
+        localStorage.setItem("count", "0");
+      }
+		} else {
+			 console.log("App will not remember todos between sessions");
+			window.id = 0;
+		}
   }
 
   render() {
     return (
       <TodoPanel
-        todos = { this.state.data }
-        addTodo = { this.addTodo.bind(this) }
-        handleCheckbox = { this.handleCheckbox.bind(this) }
+        todos = {this.state.data}
+        addTodo = {this.addTodo}
+        handleCheckbox = {this.handleCheckbox}
       />
     );
   }
